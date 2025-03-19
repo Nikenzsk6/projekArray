6497fd0bab893c5f3e8b369c4b5a4a46620bd759
Nikenzsk6 <ajaniken307@gmail.com>
2025-03-19 09:53:57 +0700

    P.1 mencari angka kemudia diganti dgn angka baru

--- src/array2d_/Percobaan1.java
+++ src/array2d_/Percobaan1.java
@@ -4,6 +4,8 @@
  */
 package array2d_;
 
+import javax.swing.JOptionPane;
+import java.util.ArrayList;
 /**
  *
  * @author HP
@@ -28,7 +30,7 @@ public class Percobaan1 {
             }
             System.out.print("\n");
         }
-        //menghitung rat-arata array
+        //Menghitung rat-arata array
         double Rata2=jml_elemen/banyak_elemen;
         
         //menampilkan
@@ -36,6 +38,65 @@ public class Percobaan1 {
         System.out.println("Banyaknya elemen pada array = "+banyak_elemen);
         System.out.println("Nilai rata-rata array = "+Rata2);
         
+        //Input angka yang dicari dgn JOptionPane
+        int Angkacari=0;
+        Angkacari=Integer.parseInt(JOptionPane.showInputDialog("Masukkan angka yang dicari"));
+        
+        //Menyimpan semua posisi angka yang ditemukan
+        ArrayList<String>posisi = new ArrayList();
+        ArrayList<Integer>baris = new ArrayList();
+        ArrayList<Integer>kolom = new ArrayList();
+        
+        //Mencari posisi angka dalam array
+        for(int d=0;d<data.length;d++){
+            for(int e=0;e<data[d].length;e++){
+                if(data[d][e]==Angkacari){
+                    posisi.add("Baris "+(d+1)+" ,Kolom "+(e+1));
+                    baris.add(d);
+                    kolom.add(e);
+                }
+            }
+        }
+        
+        //Jika angka tidak ditemukan
+        if(posisi.isEmpty()){
+            JOptionPane.showMessageDialog(null, "Posisi tidak ditemukan!");
+            return;
+        }
+        
+        //Menampilkan posisi angka yg ditemukan
+        System.out.println("Posisi angka ditemukan");
+        for(int d=0;d<posisi.size();d++){
+            System.out.println((d+1)+"."+posisi.get(d));
+        }
+        
+        //Meminta input posisi angka yang dicari
+        int pilihan=0;
+        pilihan=Integer.parseInt(JOptionPane.showInputDialog("Masukkan angka posisi yang dipilih"));
+        
+        //memastikan input angka baru valid
+        if(pilihan<0 || pilihan>posisi.size()){
+            JOptionPane.showMessageDialog(null, "Posisi tidak ditemukan");
+            return;
+        }
+        
+        //Meminta input angka baru
+        int angkaBaru=0;
+        angkaBaru=Integer.parseInt(JOptionPane.showInputDialog("Masukkan angka baru untuk mengganti"));
+        
+        //Mengubah nilai elemen array
+        data[baris.get(pilihan)][kolom.get(pilihan)]=angkaBaru;
+        
+        //Menampilkan array setelah perubahan
+        System.out.println("===Array setelah perubahan===");
+        for(int a=0;a<data.length;a++){
+            for(int b=0;b<data[a].length;b++){
+                System.out.print(data[a][b]+" ");
+            }
+            System.out.print("\n");
+        }
+        
+     
     }
     
 }
